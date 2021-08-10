46c5f0f94edf9d0fdea6fe6455620485062d41cf
Ana Judd <anajudd2015@gmail.com>
2021-08-10 15:14:39 -0500

    creates express app

---
+++ server/index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+
+// logging middleware
+const morgan = require('morgan');
+app.use(morgan('dev'));
+
+// static middleware
+app.use(express.static(path.join(__dirname, '..', 'public')))
+
+// body parser
+const bodyParser = require('body-parser');
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+// routes
+app.use('/api', require('./api'));
+
+// send HTML if nothing else
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, '..', 'public/index.html'))
+  });
+
+  // send 505
+  app.use(function (err, req, res, next) {
+    console.error(err);
+    console.error(err.stack);
+    res.status(err.status || 500).send(err.message || 'Internal server error.');
+  });
+
+  
+const port = process.env.PORT || 3000; 
+
+app.listen(port, function () {
+  console.log("Knock, knock");
+  console.log("Who's there?");
+  console.log(`Your server, listening on port ${port}`);
+});
